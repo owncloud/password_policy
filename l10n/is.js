da4a5930e8d158c89b949ebb88bc41d9603ef4fb
ownCloud Jenkins <jenkins@owncloud.com>
2018-03-02 03:00:47 +0000

    [tx-robot] updated from transifex

---
+++ l10n/is.js
@@ -0,0 +1,26 @@
+OC.L10N.register(
+    "password_policy",
+    {
+    "Saved" : "Vistað",
+    "Failed to save!" : "Mistókst að vista!",
+    "Expiration date is required" : "Gildisdagsetning er nauðsynleg",
+    "Password is too short. Minimum %d characters are required." : "Lykilorðið er of stutt. Notaðu a.m.k   %d stafi.",
+    "Password contains too few lowercase characters. Minimum %d lowercase characters are required." : "Lykilorðið inniheldur of fáa lágstafi. Krafist er að lágmarki %d lágstafa.",
+    "Password contains too few numbers. Minimum %d numbers are required." : "Lykilorðið inniheldur of fáa tölustafi. Krafist er að lágmarki %d tölustafa.",
+    "Password should be different than your last %d passwords" : "Lykilorðið ætti að vera öðruvísi en síðustu %d lykilorð",
+    "Password contains invalid special characters. Only %s are allowed." : "Lykilorð inniheldur ógilda sérstafi. Aðeins %s eru leyfðir.",
+    "Password contains too few special characters. Minimum %d special characters are required." : "Lykilorðið inniheldur of fáa sérstafi. Krafist er að lágmarki %d sérstafa.",
+    "Password contains too few uppercase characters. Minimum %d uppercase characters are required." : "Lykilorðið inniheldur of fáa hástafi. Krafist er að lágmarki %d hástafa.",
+    "Password and public link expiration policies" : "Stefna varðandi gildistíma lykilorða og opinberra tengla",
+    "Minimum password requirements for user accounts and public links:" : "Lágmarkskröfur lykilorða fyrir notendaaðganga og opinbera tengla:",
+    "different than last passwords" : "öðruvísi en síðustu lykilorð",
+    "minimum characters" : "lágmarksfjöldi stafa",
+    "lowercase letters" : "lágstafir",
+    "uppercase letters" : "hástafir",
+    "numbers" : "tölur",
+    "special characters" : "sérstafir",
+    "Define special characters:" : "Skilgreindu sérstafi:",
+    "Public link expiration policy:" : "Stefna varðandi gildistíma opinberra tengla:",
+    "Save" : "Vista"
+},
+"nplurals=2; plural=(n % 10 != 1 || n % 100 == 11);");
