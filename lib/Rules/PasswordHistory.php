6fe40f0917ddf0934e3d07aaf2c6eea67408f2ac
Juan Pablo Villafáñez <jvillafanez@solidgeargroup.com>
2018-07-13 11:33:58 +0200

    Send an expired notification when the admin expires a password

--- lib/Rules/PasswordHistory.php
+++ lib/Rules/PasswordHistory.php
@@ -63,7 +63,8 @@ class PasswordHistory extends Base {
 			$val+1
 		);
 		foreach($oldPasswords as $oldPassword) {
-			if ($this->hasher->verify($password, $oldPassword->getPassword())) {
+			if ($oldPassword->getPassword() !== 'dummy' &&
+					$this->hasher->verify($password, $oldPassword->getPassword())) {
 				throw new PolicyException(
 					$this->l10n->t('The password must be different than your previous %d passwords.', [$val]));
 			}
