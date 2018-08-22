11264a6825facf9454f3038d40500fc4c9e01e9f
Phil Davis <phil@jankaritech.com>
2018-08-22 15:14:55 +0545

    Remember errors in lint of PHP files

--- tests/travis/lint.sh
+++ tests/travis/lint.sh
@@ -2,4 +2,20 @@
 
 set -e
 
-find . -name \*.php -not -path './vendor/*' -exec php -l "{}" \;
+find . -name \*.php -not -path './vendor/*' -exec bash -c 'php -l "{}" || echo "{}">>lint-errors.txt' \;
+EXIT_STATUS=$?
+if [ ${EXIT_STATUS} -ne 0 ]
+then
+	echo "Error in find in lint.sh script"
+	exit ${EXIT_STATUS}
+fi
+
+if [ -e lint-errors.txt ]
+then
+	echo "**** Lint errors were detected in the following files:"
+	cat lint-errors.txt
+	rm lint-errors.txt
+	exit 1
+else
+	exit 0
+fi
