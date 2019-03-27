944e9c0c3ec3c48012806c035bbbd5065497a3e8
Phil Davis <phil@jankaritech.com>
2019-03-27 21:33:41 +0545

    Remove outdated PHPUnit/Autoload.php

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -25,8 +25,4 @@ require_once __DIR__ . '/../../../../lib/base.php';
 
 OC::$composerAutoloader->addPsr4('Test\\', OC::$SERVERROOT . '/tests/lib', true);
 
-if (!\class_exists('PHPUnit\Framework\TestCase')) {
-	require_once('PHPUnit/Autoload.php');
-}
-
 OC_Hook::clear();
