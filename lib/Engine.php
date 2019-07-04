62e8246e6c0b759d8868c1b09bcce5cf6836b828
Phil Davis <phil@jankaritech.com>
2019-07-04 21:27:22 +0545

    Use CHAR_SYMBOLS to generate password when no special chars are specified

--- lib/Engine.php
+++ lib/Engine.php
@@ -92,6 +92,9 @@ class Engine {
 		if ($this->yes('spv_special_chars_checked')) {
 			$val = $this->configValues['spv_special_chars_value'];
 			$chars = $this->configValues['spv_def_special_chars_value'];
+			if ($chars == '') {
+				$chars = ISecureRandom::CHAR_SYMBOLS;
+			}
 			$password .= $this->random->generate($val + 1, $chars);
 		}
 
